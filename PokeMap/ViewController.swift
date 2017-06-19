be3e42f35ebc6348354bc2b2b2bc54200575dcd5
Freaking Udemy App 2 <you@yourdomain.com>
2017-06-18 20:50:16 -0500

    first commit

---
+++ PokeMap/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PokeMap
+//
+//  Created by Jake Wojtas on 6/18/17.
+//  Copyright © 2017 Jake Wojtas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("Hey all")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
